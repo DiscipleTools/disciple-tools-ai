82288f06dcb8639f918d1705504ad2b3ed202a49
kodinkat <19330800+kodinkat@users.noreply.github.com>
2025-07-10 14:45:08 +0100

    Lists: Better search box (#36)
    * Refactor AI Filter UI and Logic
    
    * Replaced the existing search bar with a new AI prompt button for improved user interaction.
    * Updated modal functionality to handle AI filter prompts, including event listeners for input submission.
    * Enhanced translation support for the new AI prompt feature.
    * Cleaned up and optimized existing JavaScript functions related to AI filter handling.
    
    * Enhance AI Filter Button and Modal UI
    
    * Updated the AI prompt button to include a label for better user guidance.
    * Changed the button text from 'Submit' to 'Search' for clarity.
    * Improved the layout of the modal buttons for better visual alignment.
    * Added auto-focus functionality on the search field when the modal opens.

--- tile/custom-tile.php
+++ tile/custom-tile.php
@@ -205,35 +205,23 @@ class Disciple_Tools_AI_Tile
 
             #ai-search-bar button.ai-clear-button {
                 position: absolute;
-                inset-inline-end: 1.75rem;
-                top: 0.4rem;
+                inset-inline-end: 0.5rem;
+                top: 0.99rem;
                 border: 0;
                 background-color: #FFFFFF;
                 padding: 2px;
                 height: var(--search-input-height);
             }
 
-            #ai-search-bar button.ai-filter-button {
-                position: absolute;
-                inset-inline-end: 0.5rem;
-                top: 0.4rem;
-                border: 2px;
-                background-color: #FFFFFF;
-                padding: 2px;
-                height: var(--search-input-height);
-            }
             /* ===== Search & Filter ===== */
         </style>
 
         <div id="ai-search-filter">
-            <div id="ai-search-bar">
-                <input type="text" id="ai-search" placeholder="<?php esc_html_e( 'Describe list to show...', 'disciple-tools-ai' ); ?>" />
-                <button id="ai-clear-button" style="display: none;" class="ai-clear-button mdi mdi-close" onclick="clear_ai_filter();"></button>
-                <button class="ai-filter-button" onclick="create_ai_filter();">
-                  <i id="ai_filter_icon" class="mdi mdi-star-four-points-outline"></i>
-                  <span id="ai_filter_spinner" style="display: none; height: 16px; width: 16px" class="loading-spinner active"></span>
-                </button>
-            </div>
+            <button id="ai_prompt_button" class="button no-margin icon-button" style="padding: 0.1rem 0.75rem;min-height: 100%;" onclick="show_ai_prompt_modal();">
+                <i id="ai_prompt_icon" class="mdi mdi-large mdi-star-four-points-outline" style="font-size: large;"></i>
+                <span style="margin-left: 0.5rem;"><?php esc_html_e( 'Search or Filter', 'disciple-tools-ai' ); ?></span>
+                <span id="ai_prompt_spinner" style="display: none; height: 16px; width: 16px; margin-left: 0.5rem;" class="loading-spinner active"></span>
+            </button>
         </div>
 
         <script>
@@ -246,6 +234,12 @@ class Disciple_Tools_AI_Tile
                     'nonce' => wp_create_nonce( 'wp_rest' ),
                     'translations' => [
                         'custom_filter' => __( 'Custom AI Filter', 'disciple-tools-ai' ),
+                        'ai_prompt' => [
+                            'title' => __( 'AI Filter Prompt', 'disciple-tools-ai' ),
+                            'prompt_placeholder' => __( 'Describe list to show...', 'disciple-tools-ai' ),
+                            'search' => __( 'Search', 'disciple-tools-ai' ),
+                            'close_but' => __( 'Close', 'disciple-tools-ai' )
+                        ],
                         'multiple_options' => [
                             'title' => __( 'Multiple Options Detected', 'disciple-tools-ai' ),
                             'locations' => __( 'Locations', 'disciple-tools-ai' ),
@@ -269,17 +263,6 @@ class Disciple_Tools_AI_Tile
                  * Proceed with AI filter prompt setup.
                  */
 
-                document.getElementById('ai-search').addEventListener('keyup', function(e) {
-                    e.preventDefault();
-
-                    if (e.key === 'Enter') { // Enter key pressed.
-                        create_ai_filter();
-
-                    } else { // Manage field clearing option.
-                        show_ai_filter_clear_option();
-                    }
-                });
-
                 window.show_ai_filter_clear_option = () => {
                     const text = document.getElementById('ai-search').value;
                     const clear_button = document.getElementById('ai-clear-button');
@@ -296,15 +279,13 @@ class Disciple_Tools_AI_Tile
                     document.getElementById('ai-search').value = '';
                 }
 
-                window.create_ai_filter = () => {
-                    const text = document.getElementById('ai-search').value;
-
+                window.create_ai_filter = (text) => {
                     if (!text) {
                         return;
                     }
 
-                    const dt_ai_filter_prompt_spinner = $('#ai_filter_spinner');
-                    const dt_ai_filter_prompt_button = $('#ai_filter_icon');
+                    const dt_ai_filter_prompt_spinner = $('#ai_prompt_spinner');
+                    const dt_ai_filter_prompt_button = $('#ai_prompt_icon');
 
                     dt_ai_filter_prompt_button.fadeOut('fast', () => {
                         dt_ai_filter_prompt_spinner.fadeIn('slow', () => {
@@ -332,8 +313,8 @@ class Disciple_Tools_AI_Tile
                                 if (response?.status === 'error') {
                                     alert( response?.message );
 
-                                    document.getElementById('ai_filter_spinner').style.display = 'none';
-                                    document.getElementById('ai_filter_icon').style.display = 'inline-block';
+                                    document.getElementById('ai_prompt_spinner').style.display = 'none';
+                                    document.getElementById('ai_prompt_icon').style.display = 'inline-block';
 
                                 } else if ((response?.status === 'multiple_options_detected') && (response?.multiple_options)) {
                                     window.show_multiple_options_modal(response.multiple_options, response?.pii, response?.fields);
@@ -343,8 +324,8 @@ class Disciple_Tools_AI_Tile
                                     create_custom_filter(response.filter, response?.inferred, response?.text_search);
 
                                     // Stop spinning....
-                                    document.getElementById('ai_filter_spinner').style.display = 'none';
-                                    document.getElementById('ai_filter_icon').style.display = 'inline-block';
+                                    document.getElementById('ai_prompt_spinner').style.display = 'none';
+                                    document.getElementById('ai_prompt_icon').style.display = 'inline-block';
                                 }
                             })
                             .catch(error => {
@@ -360,6 +341,79 @@ class Disciple_Tools_AI_Tile
 
                         });
                     });
+                }
+
+                window.show_ai_prompt_modal = () => {
+                    const modal = $('#modal-large');
+                    if (modal) {
+                        $(modal).find('#modal-large-title').html(`${window.lodash.escape(settings.translations.ai_prompt.title)}`);
+
+                        // Add HTML to modal.
+                        const html = `
+                            <div id="ai-search-bar">
+                                <input type="text" id="ai-search" placeholder="${window.lodash.escape(settings.translations.ai_prompt.prompt_placeholder)}" />
+                                <button id="ai-clear-button" style="display: none;" class="ai-clear-button mdi mdi-close" onclick="clear_ai_filter();"></button>
+                            </div>
+                            <br>
+                            <div style="display: flex; justify-content: space-between;">
+                                <button class="button" data-close aria-label="submit" type="button" style="background-color: #f2f2f2; color: #000;">
+                                    <span aria-hidden="true">${window.lodash.escape(settings.translations.ai_prompt.close_but)}</span>
+                                </button>
+                                <button class="button" aria-label="submit" type="button" id="ai_prompt_submit">
+                                    <span aria-hidden="true">${window.lodash.escape(settings.translations.ai_prompt.search)}</span>
+                                </button>
+                            </div>
+                        `;
+                        $(modal).find('#modal-large-content').html(html);
+                        
+
+                        // Add event listeners to modal.
+                        $(document).off('open.zf.reveal', '[data-reveal]'); // Remove existing modal open listeners
+                        //auto focus on the search field
+                        $(document).on('open.zf.reveal', '[data-reveal]', function (evt) {
+                            document.getElementById('ai-search').focus();
+                        });
+
+                        $(document).on('open.zf.reveal', '[data-reveal]', function (evt) {
+                            document.getElementById('ai-search').addEventListener('keyup', function(e) {
+                                e.preventDefault();
+
+                                if (e.key === 'Enter') { // Enter key pressed.
+                                    window.handle_ai_prompt_submit(modal);
+
+                                } else { // Manage field clearing option.
+                                    window.show_ai_filter_clear_option();
+                                }
+                            });
+                        });
+
+                        // Remove existing modal close listeners
+                        $(document).off('closed.zf.reveal', '[data-reveal]');
+                        $(document).on('closed.zf.reveal', '[data-reveal]', function (evt) {
+                            console.log('closed', evt);
+                            // Remove click event listener, to avoid a build-up of event listeners.
+                            $(document).off('click', '#ai_prompt_submit');    
+                            $('#ai_prompt_submit').off('click');
+                        });
+
+                        // Remove any existing event listeners first to prevent duplicates
+                        $(document).off('click', '#ai_prompt_submit');
+                        $('#ai_prompt_submit').off('click');
+                        $(document).on('click', '#ai_prompt_submit', function (evt) {
+                            window.handle_ai_prompt_submit(modal);
+                        });
+
+                        // Open modal.
+                        $(modal).foundation('open');
+                        $(modal).css('top', '150px');
+                    }
+                }
+
+                window.handle_ai_prompt_submit = (modal) => {
+                    window.create_ai_filter( document.getElementById('ai-search').value );
+
+                    // Close modal.
+                    $(modal).foundation('close');
                 }
 
                 window.show_multiple_options_modal = (multiple_options, pii, filter_fields) => {
@@ -527,8 +581,8 @@ class Disciple_Tools_AI_Tile
                         $(modal).css('top', '150px');
 
                         $(document).on('closed.zf.reveal', '[data-reveal]', function (evt) {
-                            document.getElementById('ai_filter_spinner').style.display = 'none';
-                            document.getElementById('ai_filter_icon').style.display = 'inline-block';
+                            document.getElementById('ai_prompt_spinner').style.display = 'none';
+                            document.getElementById('ai_prompt_icon').style.display = 'inline-block';
 
                             // Remove click event listener, to avoid a build-up and duplication of modal selection submissions.
                             $(document).off('click', '#multiple_options_submit');
@@ -555,8 +609,8 @@ class Disciple_Tools_AI_Tile
                     $(modal).foundation('close');
 
                     // Ensure spinner is still spinning.
-                    document.getElementById('ai_filter_spinner').style.display = 'inline-block';
-                    document.getElementById('ai_filter_icon').style.display = 'none';
+                    document.getElementById('ai_prompt_spinner').style.display = 'inline-block';
+                    document.getElementById('ai_prompt_icon').style.display = 'none';
 
                     // Submit selections.
                     jQuery.ajax({
@@ -582,16 +636,16 @@ class Disciple_Tools_AI_Tile
                         }
 
                         // Stop spinning....
-                        document.getElementById('ai_filter_spinner').style.display = 'none';
-                        document.getElementById('ai_filter_icon').style.display = 'inline-block';
+                        document.getElementById('ai_prompt_spinner').style.display = 'none';
+                        document.getElementById('ai_prompt_icon').style.display = 'inline-block';
 
                     })
                     .fail(function (err) {
                         console.log('error')
                         console.log(err)
 
-                        document.getElementById('ai_filter_spinner').style.display = 'none';
-                        document.getElementById('ai_filter_icon').style.display = 'inline-block';
+                        document.getElementById('ai_prompt_spinner').style.display = 'none';
+                        document.getElementById('ai_prompt_icon').style.display = 'inline-block';
                     });
                 }
 
