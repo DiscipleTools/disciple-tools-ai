246c1b5b1b80bee5812bd24b498b83ce47ad4dcc
kodinkat <me@karlkatewu.com>
2025-09-09 11:00:10 +0100

    Enhance language selection for audio comment uploads
    * Added a conditional check to display the language selection dropdown only if the audio comment transcription module is enabled.
    * Maintained the comprehensive list of supported languages for improved user experience.

--- lib/disciple-tools-ai-filters.php
+++ lib/disciple-tools-ai-filters.php
@@ -20,75 +20,77 @@ function dt_upload_audio_comment( $transcription, $audio_file ): string {
 
 add_action( 'dt_upload_audio_comment_languages', 'dt_upload_audio_comment_languages', 10, 0 );
 function dt_upload_audio_comment_languages() {
-    ?>
-    <select name="locale">
-        <?php
-        $dt_user_locale = get_user_locale( get_current_user_id() );
-        $languages = dt_get_global_languages_list();
-        foreach ( $languages as $code => $language ) {
-            if ( in_array( $code, [
-                'nl',
-                'es',
-                'ko',
-                'it',
-                'de',
-                'th',
-                'ru',
-                'pt',
-                'pl',
-                'id',
-                'sv',
-                'cs',
-                'en',
-                'ja',
-                'fr',
-                'ro',
-                'tr',
-                'ca',
-                'hu',
-                'uk',
-                'el',
-                'bg',
-                'sr',
-                'mk',
-                'lv',
-                'sl',
-                'hi',
-                'gl',
-                'da',
-                'ur',
-                'sk',
-                'he',
-                'fi',
-                'az',
-                'lt',
-                'et',
-                'nn',
-                'cy',
-                'pa',
-                'fa',
-                'eu',
-                'vi',
-                'bn',
-                'ne',
-                'mr',
-                'be',
-                'kk',
-                'hy',
-                'sw',
-                'ta',
-                'sq'
-            ] ) ) {
-                ?>
-                <option
-                    value="<?php echo esc_html( $code ); ?>" <?php selected( substr( trim( $dt_user_locale ), 0, 2 ) === $code ) ?>>
-                    <?php echo esc_html( ! empty( $language['flag'] ) ? $language['flag'] . ' ' : '' ); ?> <?php echo esc_html( $language['native_name'] ); ?>
-                </option>
-                <?php
-            }
-        }
+    if ( Disciple_Tools_AI_API::has_module_value( Disciple_Tools_AI_API::$module_default_id_dt_ai_audio_comment_transcription, 'enabled', 1 ) ) {
         ?>
-    </select>
-    <br>
-    <?php
+        <select name="locale">
+            <?php
+            $dt_user_locale = get_user_locale( get_current_user_id() );
+            $languages = dt_get_global_languages_list();
+            foreach ( $languages as $code => $language ) {
+                if ( in_array( $code, [
+                    'nl',
+                    'es',
+                    'ko',
+                    'it',
+                    'de',
+                    'th',
+                    'ru',
+                    'pt',
+                    'pl',
+                    'id',
+                    'sv',
+                    'cs',
+                    'en',
+                    'ja',
+                    'fr',
+                    'ro',
+                    'tr',
+                    'ca',
+                    'hu',
+                    'uk',
+                    'el',
+                    'bg',
+                    'sr',
+                    'mk',
+                    'lv',
+                    'sl',
+                    'hi',
+                    'gl',
+                    'da',
+                    'ur',
+                    'sk',
+                    'he',
+                    'fi',
+                    'az',
+                    'lt',
+                    'et',
+                    'nn',
+                    'cy',
+                    'pa',
+                    'fa',
+                    'eu',
+                    'vi',
+                    'bn',
+                    'ne',
+                    'mr',
+                    'be',
+                    'kk',
+                    'hy',
+                    'sw',
+                    'ta',
+                    'sq'
+                ] ) ) {
+                    ?>
+                    <option
+                        value="<?php echo esc_html( $code ); ?>" <?php selected( substr( trim( $dt_user_locale ), 0, 2 ) === $code ) ?>>
+                        <?php echo esc_html( ! empty( $language['flag'] ) ? $language['flag'] . ' ' : '' ); ?> <?php echo esc_html( $language['native_name'] ); ?>
+                    </option>
+                    <?php
+                }
+            }
+            ?>
+        </select>
+        <br>
+        <?php
+    }
 }
